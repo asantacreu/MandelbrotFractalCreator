6b00108047bde33a890e0725d5712ad11aec7dd2
Aniol Santacreu <aniolsantacreu_1@hotmail.com>
2018-11-17 16:34:51 +0100

    Creates bitmap headers

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include "BitmapFileHeader.h"
+#include "BitmapInfoHeader.h"
+using namespace std;
 
-int main(int argc, char *argv[]){
-	std::cout << "Hello World!" << std::endl;
+int main() {
+    cout << "Hello World!" << endl;
+    cout << sizeof(BitmapInfoHeader) << endl;
 	return 0;
 }
