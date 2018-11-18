e7061f941df367b3960c9c62d2d41b1939577e7f
Aniol Santacreu <aniolsantacreu_1@hotmail.com>
2018-11-18 17:01:30 +0100

    Creates Mandelbrot algorithm

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <cstdint>
 #include "Bitmap.h"
+#include "Mandelbrot.h"
 
 using namespace std;
 
@@ -9,11 +11,30 @@ int main() {
     const int HEIGHT = 800;
     Bitmap bitmap(WIDTH, HEIGHT);
 
+    double min = 99999;
+    double max = -99999;
+
     for(int x = 0; x < WIDTH; x++) {
-        for(int y = 0; y < WIDTH; y++) {
-            bitmap.setPixel(x, y, 255, 0, 0);
+        for(int y = 0; y < HEIGHT; y++) {
+            double xFractal = ((x - WIDTH/2) / (WIDTH/2.0));
+            double yFractal = ((y - HEIGHT/2) / (HEIGHT/2.0));
+
+            int iterations = Mandelbrot::getIterations(xFractal, yFractal);
+
+            uint8_t red = (uint8_t)(256 * (double)iterations / Mandelbrot::MAX_ITERATIONS);
+
+            bitmap.setPixel(x, y, red, 0, 0);
+            if(red < min){
+                min = red;
+            }
+            if(red > max){
+                max = red;
+            }
+
         }
     }
+
+    cout << min << ", " << max << endl;
 
     bool bitmapWritten = bitmap.write("test1.bmp");
 
